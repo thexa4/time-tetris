dbd1c144cd8da530a259d187cbfde7ffc319c291
Derkje-J <derk-jan@karrenbeld.info>
2012-10-27 02:35:21 +0200

    New Assembly title

--- TimeTetris/TimeTetris/Properties/AssemblyInfo.cs
+++ TimeTetris/TimeTetris/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeTetris")]
+[assembly: AssemblyTitle("TimeTetris (Reach)")]
 [assembly: AssemblyProduct("TimeTetris")]
 [assembly: AssemblyDescription("Game Programming Practical 2")]
 [assembly: AssemblyCompany("Derk-Jan Karrenbeld & Max Maton")]
